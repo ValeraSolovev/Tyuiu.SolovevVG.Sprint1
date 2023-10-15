4808605def37114b489634f040e86ae11b9cf14b
SolovevVG <Tyuiu.SolovevVG@outlook.com>
2023-10-15 21:25:02 +0500

    Add Task5

---
+++ Tyuiu.SolovevVG.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SolovevVG.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.SolovevVG.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double f = 60;
+            DataService ds = new DataService();
+            double res = ds.AngleToHoursMinutes(f);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 2;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.SolovevVG.Sprint1.Task5.V7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SolovevVG.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.SolovevVG.Sprint1.Task5.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Соловьев В.Г. | СМАРТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Соловьев В.Г. | СМАРТБ-23-1                                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая решает следующую задачу,                    *");
+            Console.WriteLine("* Определить h – полное количество часов прошедших от начала суток до .   *");
+            Console.WriteLine("* того момента (в первой половине дня), когда часовая стрелка повернулась *");
+            Console.WriteLine("* на f градусов (0<f<360, f – вещественное число)                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение F");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = Convert.ToInt32(ds.AngleToHoursMinutes(x));
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
